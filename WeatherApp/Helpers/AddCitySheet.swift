078e67766d398c9f12a419768562de3c6af441e7
corb0140 <corb0140@algonquinlive.com>
2024-12-01 00:01:49 -0500

    working on fetching, add sheet done.

---
+++ WeatherApp/Helpers/AddCitySheet.swift
@@ -0,0 +1,18 @@
+//
+//  AddCitySheet.swift
+//  WeatherApp
+//
+//  Created by Mark Corbin on 2024-11-30.
+//
+
+import SwiftUI
+
+struct AddCitySheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddCitySheet()
+}
