fcd57922dc901e80a40e2d28d2d966b70bd3c255
corb0140 <corb0140@algonquinlive.com>
2024-12-01 21:10:16 -0500

    created map with hard coded values and graident overlay. will try to pass coord for map from fetch.

---
+++ WeatherApp/Helpers/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  WeatherApp
+//
+//  Created by Mark Corbin on 2024-12-01.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
